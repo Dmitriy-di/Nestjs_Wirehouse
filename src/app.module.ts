a16c8ae32ddfbffbe0abbea29bbfa4aaec82e192
Dmitriy Korolev <you@example.com>
2023-07-08 17:17:06 +0300

    Подключение бд mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CategoryModule } from './category/category.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './category/entities/category.entity';
 
 @Module({
   imports: [
@@ -13,6 +16,17 @@ import { CategoryModule } from './category/category.module';
       autoSchemaFile: true,
     }),
     CategoryModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Category],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryResolver } from './category.resolver';
+import { Category } from './entities/category.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [CategoryResolver, CategoryService]
+  providers: [CategoryResolver, CategoryService],
+  imports: [TypeOrmModule.forFeature([Category])],
 })
 export class CategoryModule {}
